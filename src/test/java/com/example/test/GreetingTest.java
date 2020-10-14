861fd632d64a6f4f08bb527d78c87e8835e6ce17
Aneirine <anyashi2002@gmail.com>
2020-10-14 11:27:13 +0300

    Fix pom

--- src/test/java/com/example/test/GreetingTest.java
+++ src/test/java/com/example/test/GreetingTest.java
@@ -1,21 +1,26 @@
 package com.example.test;
 
 import com.example.test.greeting.Greeting;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GreetingTest {
 
+    Greeting greeting;
 
+    @BeforeEach
+    void setUp() {
+        System.out.println("Before each");
+        greeting = new Greeting();
+    }
 
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld());
     }
 
     @Test
     void helloWorld1() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld("John"));
     }
 }
